75f140d8c11d7b49e71f504caf057e14e0846f63
siseldyke <siseldyke@gmail.com>
2024-11-19 13:49:43 -0500

    completed merge requests and did some small cleanup

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom"
 
 const Landing = () => {
   return (
-    <main class="landing-container">
-      <h1 class="landing-title">Self Learner App</h1>
-      <ul class="mission-statement">
+    <main className="landing-container">
+      <h1 className="landing-title">Self Learner App</h1>
+      <ul className="mission-statement">
         <li><em>Analyze</em> your life goals and interests</li>
         <li>Be <em>Gifted</em> New Paths and Ideas</li>
         <li><em>Linked</em> with real people at all depths of the Great Forest</li>
       </ul>
       <nav>
-        <ul class="signUp-In">
+        <ul className="signUp-In">
           <li><Link to="/signin">Sign In</Link></li>
           <li><Link to="/signup">Sign Up</Link></li>
         </ul>
