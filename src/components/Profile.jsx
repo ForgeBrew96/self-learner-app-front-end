286e40bf160eacabf56a02b934a0404ca81f2fa9
siseldyke <siseldyke@gmail.com>
2024-11-20 13:09:53 -0500

    added effects to buttons as well as new tab function

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,17 +1,19 @@
 const Profile = ({user}) => {
-
+  
     return (
       <main className="userProfile">
         <div className="profileHeader">
           <h2>Profile for {user.username}</h2>
-          <img className="profilePicture"></img>
+          <img className="profilePicture" alt='profilePic'></img>
           <h3>Badges Earned</h3>
           <h3>Communites joined</h3>
         </div>
         <div className="userDetails">
           <p>user bio</p>
-          <li>interest tag 1</li>
-          <li>interest tag 2</li>
+          <li>{user.fitnessPoints}</li>
+          <li>{user.videoGamesPoints}</li>
+          <li>{user.boardGamesPoints}</li>
+          <li>{user.musicInsPoints}</li>
         </div>
         <div className="userCarousel">
           <p>assorted links to badges activity communities</p>
