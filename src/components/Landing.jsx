6aaa18de08977e8192dc49272583a6dbb06ee9b5
XOKEESH <hello@kishamartin.com>
2024-11-20 06:17:00 -0800

    Landing.css

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom"
+import './Landing.css'
 
 const Landing = () => {
   return (
     <main className="landing-container">
-      <h1 className="landing-title">Self Learner App</h1>
-      <ul className="mission-statement">
+      <img className="logo" src="public/img/Sprout (1).png" alt="App Logo" />
+      <h1 className="landing-title">Sprout</h1>
+      {/* <ul className="mission-statement">
         <li><em>Analyze</em> your life goals and interests</li>
         <li>Be <em>Gifted</em> New Paths and Ideas</li>
         <li><em>Linked</em> with real people at all depths of the Great Forest</li>
-      </ul>
+      </ul> */}
+      <p>Discover your passions. Set meaningful goals. Join a community built for growth and self discovery.</p>
       <nav>
         <ul className="signUp-In">
           <li><Link to="/signin">Sign In</Link></li>
