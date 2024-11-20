4250810c1f95e18d11650e1aa09a8a65759d6c58
siseldyke <siseldyke@gmail.com>
2024-11-20 16:26:47 -0500

    styling updates

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,19 +1,24 @@
+import './Profile.css'
+
+
 const Profile = ({user}) => {
   
     return (
       <main className="userProfile">
-        <div className="profileHeader">
+        <div className="profileOwner">
           <h2>Profile for {user.username}</h2>
+        </div>
+        <div className="profileHeader">
           <img className="profilePicture" alt='profilePic'></img>
-          <h3>Badges Earned</h3>
-          <h3>Communites joined</h3>
+          <h3>Badges Earned:</h3>
+          <h3>Communites joined:</h3>
         </div>
         <div className="userDetails">
-          <p>user bio</p>
-          <li>{user.fitnessPoints}</li>
-          <li>{user.videoGamesPoints}</li>
-          <li>{user.boardGamesPoints}</li>
-          <li>{user.musicInsPoints}</li>
+          <p>User Bio</p>
+          <li>Fitness Points:{user.fitnessPoints}</li>
+          <li>Video Game Points:{user.videoGamesPoints}</li>
+          <li>Board Game Points:{user.boardGamesPoints}</li>
+          <li>Music Points:{user.musicInsPoints}</li>
         </div>
         <div className="userCarousel">
           <p>assorted links to badges activity communities</p>
