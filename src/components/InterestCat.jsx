f15804c80958bcd6c5ee20051ac0a6448c61b5f8
siseldyke <siseldyke@gmail.com>
2024-11-20 10:01:57 -0500

    added youtube video links to interestcat

--- src/components/InterestCat.jsx
+++ src/components/InterestCat.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import * as interestCatService from '../service/interestCatService.js'
+import { Link } from "react-router-dom";
+
 
 const interestCat = () => {
   const [interestCatList, setInterestCat] = useState([]);
@@ -8,7 +10,7 @@ useEffect(() => {
  const FetchInterestCats = async () => {
   try{
   const categories = await interestCatService.index()
-
+    
   if (categories.error) {
     throw new Error(categories.error)
   }
@@ -24,6 +26,7 @@ useEffect(() => {
 const categories = interestCatList.map((category) =>
   <li key={category._id}>
     <h3>{category.name}</h3>
+    <Link to ={category.link}><button>see more</button></Link>
   </li>
 )
 
